cae0042873b17484a11e20d90f542554466171b9
yuta <127507562+yuta2710@users.noreply.github.com>
2023-08-14 13:18:40 +0700

    MOVE TO FOLDER BASIC

--- basic/main.go
+++ basic/main.go
@@ -263,6 +263,49 @@ func main() {
 	bill.updateTip(10)
 
 	fmt.Println(bill.format())
+
+	shapes := []shape{
+		square{length: 15.2},
+		circle{radius: 700},
+	}
+
+	for _, v := range shapes {
+		printShapeInfo(v)
+		fmt.Println("---")
+	}
+}
+
+type shape interface {
+	area() float64
+	circumf() float64
+}
+type square struct {
+	length float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (s square) area() float64 {
+	return s.length * s.length
+}
+
+func (s square) circumf() float64 {
+	return s.length * 4
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) circumf() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func printShapeInfo(s shape) {
+	fmt.Printf("Area of %T is: %0.2f \n", s, s.area())
+	fmt.Printf("Circumreference of %T is : %0.2f \n", s, s.circumf())
 }
 
 func updateName(x string) {
